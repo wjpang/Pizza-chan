13bd21b2a840714b54294c8df19adfe19e05bea1
Vielor <pangwenjuin@gmail.com>
2023-08-27 23:22:36 +1000

    Tidying colors

--- debugging_tools/colours/colours.py
+++ debugging_tools/colours/colours.py
@@ -1,10 +1,10 @@
 import itertools
 
 # Generate all possible color combinations in RGB mode
-colors = list(itertools.product(range(0, 256, 10), range(0, 256, 10), range(0, 256, 10)))
+colors = list(itertools.product(range(0, 256, 8), range(0, 256, 8), range(0, 256, 8)))
 
 # Open a file for writing
-with open("color_combinations_reduced10.txt", "w") as f:
+with open("color_combinations_8.txt", "w", encoding="utf-8") as f:
     # Write each color combination to the file
     f.write("apply_random_country_color = {\n\trestore_country_color = yes\n\trandom_list={")
     for color in colors:
