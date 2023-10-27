e3b1b356e0f59c1d192da0981040562c27f03010
Melvasul <alessandrotimpone@gmail.com>
2023-10-27 13:20:03 +0200

    uppy

--- debugging_tools/localization.py
+++ debugging_tools/localization.py
@@ -9,8 +9,9 @@ def start():
     mod_path = r"C:\Users\AlexI\OneDrive\Documenti\Paradox Interactive\Europa Universalis IV\mod"
     # mod = mod_path + r"\Ages-and-Splendor-Expaded"
     # mod = mod_path + r"\FlavourEventsExpanded"
-    # mod = mod_path + r"\Historical-Ideas-Expanded"
-    mod = mod_path + r"\Monuments-Expanded"
+    mod = mod_path + r"\Historical-Ideas-Expanded"
+    # mod = mod_path + r"\Monuments-Expanded"
+    mod = r"A:\Program Files (x86)\Steam\steamapps\workshop\content\236850\1770950522"
     localisation_dir = mod + r"\localisation"
     filenames = glob.glob(localisation_dir + r"\*_l_english.yml")
     output_string = ["l_english", "l_french", "l_german", "l_spanish"]
@@ -30,13 +31,14 @@ def start():
     for file in filenames:
         with open(file, "r", encoding="utf8") as f:
             data = f.read()
+            data = data.replace('\ufeff', '')
             for current_lang, next_lang in zip(output_string, output_string[1:] + [output_string[0]]):  # noqa
                 data = re.sub(current_lang + r":", next_lang + ":", data)
                 final_folder = localisation_dir + rf"\{language_folders[next_lang]}"
                 output_file = final_folder + \
                     fr"\{basename(file).split('l_english.yml')[0]}{next_lang}.yml"
                 if next_lang != "l_english":
-                    with open(output_file, "w", encoding="utf-8-sig") as out_file:
+                    with open(output_file, "w", encoding="utf-8-sig", newline="\n") as out_file:
                         out_file.write(data)
                 else:
                     break
