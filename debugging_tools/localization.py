091a7b27091be201339c35cc788cd520a5e3f2bc
Melvasul <alessandrotimpone@gmail.com>
2024-01-17 17:22:11 +0100

    localization.py is now recursive in all mods.

--- debugging_tools/localization.py
+++ debugging_tools/localization.py
@@ -1,42 +1,28 @@
-import glob
 import os
-import re
+import glob
 from os.path import basename
+import re
 
 
-def start():
-    """Create the localisation folders and files for french/german/spanish"""
-    mod_path = r"C:\Users\AlexI\OneDrive\Documenti\Paradox Interactive\Europa Universalis IV\mod"
-    # mod = mod_path + r"\Ages-and-Splendor-Expaded"
-    # mod = mod_path + r"\FlavourEventsExpanded"
-    mod = mod_path + r"\Historical-Ideas-Expanded"
-    # mod = mod_path + r"\Monuments-Expanded"
-    # mod = r"A:\Program Files (x86)\Steam\steamapps\workshop\content\236850\1770950522"
-    localisation_dir = mod + r"\localisation"
-    filenames = glob.glob(localisation_dir + r"\*_l_english.yml")
-    output_string = ["l_english", "l_french", "l_german", "l_spanish"]
-    language_folders = {
-        "l_english": "english",
-        "l_french": "french",
-        "l_german": "german",
-        "l_spanish": "spanish"
-    }
+def process_mod(mod_path, languages):
+    """Process a single mod and create localized files for specified languages"""
+    localisation_dir = os.path.join(mod_path, "localisation")
+    filenames = glob.glob(os.path.join(localisation_dir, "*_l_english.yml"))
 
-    # Create language folders if they don't exist
-    for lang_folder in language_folders.values():
+    for lang_folder in languages.values():
         folder_path = os.path.join(localisation_dir, lang_folder)
         if not os.path.exists(folder_path) and lang_folder != "english":
             os.makedirs(folder_path)
 
+    lang_list = list(languages)  # Convert dictionary keys to a list
     for file in filenames:
         with open(file, "r", encoding="utf8") as f:
             data = f.read()
             data = data.replace('\ufeff', '')
-            for current_lang, next_lang in zip(output_string, output_string[1:] + [output_string[0]]):  # noqa
+            for current_lang, next_lang in zip(lang_list, lang_list[1:] + [lang_list[0]]):
                 data = re.sub(current_lang + r":", next_lang + ":", data)
-                final_folder = localisation_dir + rf"\{language_folders[next_lang]}"
-                output_file = final_folder + \
-                    fr"\{basename(file).split('l_english.yml')[0]}{next_lang}.yml"
+                final_folder = os.path.join(localisation_dir, languages[next_lang])
+                output_file = os.path.join(final_folder, f"{basename(file).split('l_english.yml')[0]}{next_lang}.yml")
                 if next_lang != "l_english":
                     with open(output_file, "w", encoding="utf-8-sig", newline="\n") as out_file:
                         out_file.write(data)
@@ -44,4 +30,28 @@ def start():
                     break
 
 
+def start():
+    """Create the localisation folders and files for various mods and languages"""
+    mod_path = r"C:\Users\AlexI\OneDrive\Documenti\Paradox Interactive\Europa Universalis IV\mod"
+
+    mod_paths = [
+        os.path.join(mod_path, "Ages-and-Splendor-Expaded"),
+        os.path.join(mod_path, "FlavourEventsExpanded"),
+        os.path.join(mod_path, "Historical-Ideas-Expanded"),
+        os.path.join(mod_path, "Monuments-Expanded"),
+        # Add more mod paths as needed
+    ]
+
+    languages = {
+        "l_english": "english",
+        "l_french": "french",
+        "l_german": "german",
+        "l_spanish": "spanish",
+    }
+
+    for mod_path in mod_paths:
+        process_mod(mod_path, languages)
+
+
+# Run the script
 start()
